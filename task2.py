b87aef0b969f28beee2ac1948f94e6ed0bbd7e56
lpesternikova <lpesternikova@gmail.com>
2024-11-02 21:41:30 +0500

    task2

--- task2.py
+++ task2.py
@@ -13,3 +13,18 @@
 # [1, 2, 3, 4, 5]
 # Передаваемая функция: функция умножения
 # результат : 120
+
+import math
+
+_lst = [1, 2, 3, 4, 5]
+lst1 = 0
+def aggregate(func: callable, _lst: int):
+    lst1 = func(_lst)
+    return lst1
+
+#operations = sum
+operations = math.prod
+res = aggregate(operations, _lst)
+print(res)
+
+
